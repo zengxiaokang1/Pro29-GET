42211e18f53377542840550f4c3bd227fe76144e
peter <peter@atguigu.com>
2019-05-21 19:41:27 +0800

    准备测试数据

--- src/main/java/com/atguigu/git/Hello.java
+++ src/main/java/com/atguigu/git/Hello.java
@@ -2,4 +2,18 @@ package com.atguigu.git;
 
 public class Hello {
 
+	public static void main(String[] args) {
+		System.out.println("hello git!!!!");
+		System.out.println("hello git!!!!");
+		System.out.println("hello git!!!!");
+		System.out.println("hello git!!!!");
+		System.out.println("hello git!!!!");
+		System.out.println("hello git!!!!");
+		System.out.println("hello git!!!!");
+		System.out.println("hello git!!!!");
+		System.out.println("hello git!!!!");
+		System.out.println("hello git!!!!");
+		System.out.println("hello git!!!!");
+		System.out.println("hello git!!!!");
+	}
 }
