a2c392d5564815cb2e48689ed767d4da99cc5674
peter <peter@atguigu.com>
2019-05-21 19:50:43 +0800

    测试常规操作

--- src/main/java/com/atguigu/git/Hello.java
+++ src/main/java/com/atguigu/git/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("hello git!!!!");
 		System.out.println("hello git!!!!");
 		System.out.println("hello git!!!!");
-		System.out.println("hello git!!!!");
+		System.out.println("hello git!!!!");//edit by 29
 		System.out.println("hello git!!!!");
 		System.out.println("hello git!!!!");
 		System.out.println("hello git!!!!");
